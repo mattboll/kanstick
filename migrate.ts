fd5aa4af21908076e8911123e7743205ffc7f2e9
Matthieu Bollot <m.bollot@code-troopers.com>
2025-02-24 09:11:35 +0100

    feat(db): Prepare for data migrations

---
+++ migrate.ts
@@ -0,0 +1,30 @@
+import { migrate } from "postgres-migrations";
+import { Client } from "pg";
+import dotenv from "dotenv";
+
+const env = process.env.NODE_ENV || "local";
+
+dotenv.config({ path: `.env.${env}` });
+
+async function runMigrations() {
+  const client = new Client({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: Number(process.env.DB_PORT),
+  });
+
+  try {
+    await client.connect();
+    await migrate({ client }, "migrations");
+    console.log("Migrations exécutées avec succès");
+  } catch (error) {
+    console.error("Erreur lors de l'exécution des migrations :", error);
+    process.exit(1); // Arrête le processus en cas d'erreur
+  } finally {
+    await client.end();
+  }
+}
+
+runMigrations();
